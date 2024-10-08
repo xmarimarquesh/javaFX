bebbc218ef01c4052a8bb7fd602071f359820a50
xmarimarquesh <marimarquesh39@gmail.com>
2024-10-08 11:48:32 -0300

    first commit

---
+++ src/main/java/com/desktopapp/MainController.java
@@ -0,0 +1,24 @@
+package com.desktopapp;
+
+import java.net.URL;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+public class MainController {
+
+    public static Scene CreateScene() throws Exception
+    {
+        URL sceneUrl = MainController.class.getResource("MainScreen.fxml");
+        Parent root = FXMLLoader.load(sceneUrl);
+        Scene scene = new Scene(root);
+        return scene;
+    }
+
+    @FXML
+    protected Button btMain;
+
+}
